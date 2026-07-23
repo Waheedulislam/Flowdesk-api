4ff6f440c10d8d0aa197ee62617c15a17752902d
Waheedul Islam <waheeduli81@gmail.com>
2026-07-23 19:27:43 +0600

    feat(auth): add initial authentication service with Prisma

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -2,7 +2,22 @@ import prisma from "../../../config/prisma";
 import { IRegisterUser } from "./auth.interface";
 
 const registerUser = async (payload: IRegisterUser) => {
-  const isUserExists = await prisma.user;
+  const { name, email, password } = payload;
+
+  // Check if user already exists
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    throw new Error("User already exists");
+  }
+
+  // Hash password
+  // const hashPassword=await
+  // const isUserExists = await prisma.user.create();
 };
 
 export const AuthService = {
