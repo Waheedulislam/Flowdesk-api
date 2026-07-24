96b3cf25a23d19e7ea6caf1009eba794717a4273
Waheedul Islam <waheeduli81@gmail.com>
2026-07-25 02:06:03 +0600

    feat(auth): add user registration controller

---
+++ src/app/modules/auth/atuth.controller.ts
@@ -0,0 +1,20 @@
+import catchAsync from "../../../utils/catchAsync";
+import { sendResponse } from "../../../utils/sendResponse";
+import { AuthService } from "./auth.service";
+import httpStatus from "http-status-codes";
+
+const registerUser = catchAsync(async (req, res) => {
+  //1. Get register data from the client
+  const payload = req.body;
+
+  //2. Send the business logic to services
+  const result = await AuthService.registerUser(payload);
+
+  //3. Providing successful response to client
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: "User registered successfully",
+    data: result,
+  });
+});
