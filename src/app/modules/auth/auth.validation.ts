0ad3f9eb43808456ccd6805c3aaaafaf013bfc3c
Waheedul Islam <waheeduli81@gmail.com>
2026-07-25 03:16:02 +0600

    feat(validation): add reusable request validation middleware

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -1,15 +1,19 @@
 import z from "zod";
 
 const registerSchema = z.object({
-  name: z
-    .string()
-    .min(3, "Name must be at least 3 characters")
-    .max(50, "Name cannot exceed 50 characters"),
-  email: z.email("Invalid email address"),
-  password: z
-    .string()
-    .min(8, "Password must be at least 8 characters")
-    .max(100, "Password cannot exceed 100 characters"),
+  body: z.object({
+    name: z
+      .string()
+      .min(3, "Name must be at least 3 characters")
+      .max(50, "Name cannot exceed 50 characters"),
+
+    email: z.email("Invalid email address").trim().toLowerCase(),
+
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .max(100, "Password cannot exceed 100 characters"),
+  }),
 });
 
 const loginValidationSchema = z.object({
