10ab391b90d18da4f3efc4c12f7fc38fd57ccd6b
Waheedul Islam <waheeduli81@gmail.com>
2026-08-09 15:48:53 +0600

    feat(socket): setup socket.io server

---
+++ src/socket/socket.ts
@@ -0,0 +1,21 @@
+import { Server as HttpServer } from "http";
+import { Server } from "socket.io";
+
+export const initializeSocket = (server: HttpServer) => {
+  const io = new Server(server, {
+    cors: {
+      origin: process.env.CLIENT_URL,
+      credentials: true,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log(`🔌 Socket connected: ${socket.id}`);
+
+    socket.on("disconnect", () => {
+      console.log(`❌ Socket disconnected: ${socket.id}`);
+    });
+  });
+
+  return io;
+};
